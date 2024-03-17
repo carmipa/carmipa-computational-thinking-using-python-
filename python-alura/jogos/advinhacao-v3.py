e81e5332a01b36eaeb8de7e4cbf8745900d170ee
carmipa <pabcarminati@gmail.com>
2024-03-17 12:46:04 -0300

    chamada de metrodo, menor, maior etc

--- python-alura/jogos/advinhacao-v3.py
+++ python-alura/jogos/advinhacao-v3.py
@@ -7,13 +7,15 @@ numeroSecreto = 42
 chute = int(input("Digite o seu número:"))
 
 acertou = numeroSecreto == chute
+maior = chute = chute > numeroSecreto
+menor = chute < numeroSecreto
 
 if (acertou):
     print("Você acertou, chute!",chute_str)
 else:
-    if(chute > numeroSecreto):
+    if(maior):
         print("Você errou! seu chute foi maior que o número secreto.", chute)
-    elif(chute < numeroSecreto):
+    elif(menor):
         print("Você errou!, seu chute foi menor que o numero secreto." , chute)
 
     
