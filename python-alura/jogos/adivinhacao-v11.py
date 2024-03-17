c1690c71a1960bdd1a5309090cb265c0e8a905cb
carmipa <pabcarminati@gmail.com>
2024-03-17 14:51:58 -0300

    colocando pontuacao no jogo

---
+++ python-alura/jogos/adivinhacao-v11.py
@@ -0,0 +1,44 @@
+import random
+
+print("************************************")
+print("* Bem vindo ao jogo de Adivinhação *")
+print("************************************")
+
+numeroSecreto = round(random.randrange(1, 101)) #gera número entre 1 e 100
+int(numeroSecreto)
+totalTentativas = 0
+
+print("Qual nível de dificulade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == "1"):
+    totalTentativas = 20
+elif(nivel == "2"):
+    totalTentativas = 10
+else:
+    totalTentativas = 5
+
+for rodada in range (1, totalTentativas + 1):
+    print("Tentativa {} de {}".format(rodada, totalTentativas))
+    chute = int(input("Digite número entre 1 e 100:"))
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = chute == numeroSecreto
+    maior   = chute <  numeroSecreto
+    menor   = chute <  numeroSecreto
+
+    if (acertou):
+        print("Você acertou, chute!",chute)
+        break
+    else:
+        if(maior):
+            print("Você errou! seu chute foi maior que o número secreto.", chute)
+        elif(menor):
+            print("Você errou!, seu chute foi menor que o numero secreto." , chute)
+    
+print("Fim do jogo! ", "\nA resposta correta éra:", numeroSecreto)
