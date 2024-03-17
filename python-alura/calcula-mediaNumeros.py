2410afc7e51758a949db9d0b4ade1a5ca3e2aebf
carmipa <pabcarminati@gmail.com>
2024-03-17 11:53:56 -0300

    pasta com aulas da alura

---
+++ python-alura/calcula-mediaNumeros.py
@@ -0,0 +1,9 @@
+nume1 = 10
+nume2 = 15.5
+nume3 = 20.4
+nume4 = 2.4
+num5 = 6
+
+media = (nume1 + nume2 + nume3 + nume4 + num5) / 10
+
+print ("Média dos notas é:", media)
---
+++ python-alura/recebeNumero-voltaDobro.py
@@ -0,0 +1,5 @@
+numero = int(input("Número:"))
+
+dobro = numero * 2
+
+print("O dobro do número digitado é: ", dobro)
