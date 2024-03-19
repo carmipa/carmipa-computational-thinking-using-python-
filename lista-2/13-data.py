444d0bf1b717f93168b74cdcd77feda487791fc9
carmipa <pabcarminati@gmail.com>
2024-03-19 19:13:48 -0300

    exercicio 14

--- lista-2/13-data.py
+++ lista-2/13-data.py
@@ -1,28 +1,30 @@
-print(".............................................................")
-dia = int(input("Dia: "))
-mes = int(input("Mês: "))
-ano = int(input("Ano: "))
-print(".............................................................")
-print("Regra:", 
-      "\n1- dia, mês e ano devem ser números inteiros",
-      "\n2- O ano deve ser digitado com 4 digitos",
-      "\n3- O mês deve ser digitado o número")
-print(".............................................................")
+while True:
+    print(".............................................................")
+    print("Regra:", 
+        "\n1- dia, mês e ano devem ser números inteiros",
+        "\n2- O ano deve ser digitado com 4 digitos",
+        "\n3- O mês deve ser digitado o número")
 
-if(dia > 0 and dia <= 31):
-    print("O dia está correto!")
-else:
-    print("Dia digitado de forma errada!")
+    print(".............................................................")
+    dia = int(input("Dia: "))
+    mes = int(input("Mês: "))
+    ano = int(input("Ano: "))
+    print(".............................................................")
+    
+    if(dia > 0 and dia <= 31):
+        print("O dia está correto!")
+    elif(dia <= 0 and dia >= 31):
+        print("Dia digitado de forma errada!")
 
-if(mes > 0 and mes <= 12):
-    ("O mês está correto")
-else:
-    print("mês digitado de forma errada!")
+    if(mes > 0 and mes <= 12):
+        ("O mês está correto")
+    elif(mes <=0 and mes >12):
+        print("mês digitado de forma errada!")
 
-if(ano >0 and ano <= 9999):
-    ("O ano está correto")
-else:
-    print("Ano está digitado de forma incorreta!")
+    if(ano >0 and ano <= 9999):
+        ("O ano está correto")
+    elif(ano <= 0 and ano > 9999):
+        print("Ano está digitado de forma incorreta!")
     
     
     
