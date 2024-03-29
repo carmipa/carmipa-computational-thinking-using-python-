7445114830fcf61d87a5b1d073dfdf02872020fd
carmipa <pabcarminati@gmail.com>
2024-03-29 11:15:07 -0300

    lista3

---
+++ lista-3/1-calcula-sequencia-pares.py
@@ -0,0 +1,18 @@
+
+while True:
+    n1 = 1
+    n2 = 2
+    n3 = 3
+    n4 = 4
+    n5 = 5
+    n6 = 0
+
+    total = n1 + n2 + n3 + n4 + n5 + n6
+
+    print(total)
+    break
+
+
+
+
+
---
+++ lista-3/2-media-turma.py
@@ -0,0 +1,4 @@
+quantidade = int("Digite quantos aluno tem na sala: ")
+
+while (quantidade == 0):
+    
