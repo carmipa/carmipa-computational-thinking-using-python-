3f761c6df8c89e76538d2bb7e910e1237bfa7daa
carmipa <pabcarminati@gmail.com>
2024-04-01 08:47:58 -0300

    new

--- lista-3/2-media-turma.py
+++ lista-3/2-media-turma.py
@@ -1,4 +1,8 @@
 quantidade = int("Digite quantos aluno tem na sala: ")
-
+notas = int ("digite a nota: ")
 while (quantidade == 0):
     
+    
+    nota = nota + quantidade
+    media = nota / quantidade
+    print("A média da classe é:", media)
