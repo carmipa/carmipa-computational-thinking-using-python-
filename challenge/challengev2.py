8e322183c82c4673de27963ae8f8e796486ab775
carmipa <pabcarminati@gmail.com>
2024-04-12 19:19:39 -0300

    manu 4 inicio

--- challenge/challengev2.py
+++ challenge/challengev2.py
@@ -1,8 +1,21 @@
+#########################################################################################################################################
+#
 # INSTRUÇÕES DE USO:
-# quando houver uma opção de pesquisa no menu use as seguinte opções:
-# ex: 1 ou 2 ou 3 - "sempre um destes 3 código"
-
+#
+# 1 - quando houver uma opção de pesquisa no menu use as seguinte opções:
+#     ex: 1 ou 2 ou 3 - "sempre um destes 3 código"
+#
+# 2 - O programa usa a biblioteca "COLORAMA", como visto no importe, para que funcione corretamente use
+#     o seguinte comando no console abaixo antes de rodar o programa:
+#     "pip install colorama"
+#     OBS: se a biblioteca já estiver instalada, não é necessário a instalar
+#
+#########################################################################################################################################
 import os
+import colorama
+
+colorama.init()
+
 while True:
     os.system('cls')
     print("**************************************** PORTO SEGURO / OFICINA ON-LINE ****************************************")
@@ -16,11 +29,13 @@ while True:
 
     print("* 0 - SAIR DO SISTEMA")
     print("*")
-    opcao = int(input("* DIGITE A OPÇÃO DESEJADA: "))
+    opcao = input("* DIGITE A OPÇÃO DESEJADA: ")
     print("*")
     print("****************************************************************************************************************")
 
-    if(opcao == 1):
+#########################################################################################################################################
+
+    if(opcao == "1"):
         os.system('cls')
         while True:
             print("**************************************** CADASTRAR NOVOS CLIENTES ****************************************")
@@ -28,8 +43,8 @@ while True:
             print("1 - CADASTRAR NOVO CLIENTE")
             print("0 - VOLTAR AO MENU ANTERIOR")
             print("\n")
-            opcao = int(input("DIGITE A OPÇÃO DESEJADA: "))
-            if(opcao == 1):
+            opcao = input("DIGITE A OPÇÃO DESEJADA: ")
+            if(opcao == "1"):
                 print("**************************************** CADASTRAR NOVO CLIENTE ****************************************")
                 print("DADOS DO CLIENTE")
                 nomeCliente = input("NOME:...............: ")
@@ -37,22 +52,23 @@ while True:
                 profissaoCliente = input("PROFISSÃO...........: ")
                 cpfCliente = input("CPF.................: ")
                 nascionalidadeCliente = input("NASCIONALIDADE......: ")
+                print("\n")
                 print("ENDEREÇO DO CLIENTE")
                 ruaCliente = input("RUA.................: ")
                 numeroCliente = input("NUMERO..............: ")
                 cepCliente = input("CEP.................: ")
                 estadoCliente = input("ESTADO..............: ")
                 complementoCliente = input("COMPLEMENTO.........: ")
+
                 os.system('cls')
-                print("**************************************** DADOS DO CLIENTE CADASTRADOS COM SUCESSO ****************************************")
+
+                print(colorama.Fore.RED +"**************************************** DADOS DO CLIENTE CADASTRADOS COM SUCESSO ****************************************"+ colorama.Style.RESET_ALL)
                 print("\n")
-                print("DADOS DO CLIENTE")
                 print("NOME:...............: ",nomeCliente)
                 print("DATA DE NASCIMENTO..: ",dataNascimento)
                 print("PROFISSÃO...........: ",profissaoCliente)
                 print("CPF.................: ",cpfCliente)
                 print("NASCIONALIDADE......: ",nascionalidadeCliente)
-                print("ENDEREÇO DO CLIENTE")
                 print("RUA.................: ",ruaCliente)
                 print("NUMERO..............: ",numeroCliente)
                 print("CEP.................: ",cepCliente)
@@ -60,12 +76,14 @@ while True:
                 print("COMPLEMENTO.........: ",complementoCliente)
                 print("\n")
 
-            elif(opcao == 0):
+            elif(opcao == "0"):
                 break
             else:
-                print("OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!")
+                print(colorama.Fore.RED +"OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!" + colorama.Style.RESET_ALL)
 
-    elif(opcao == 2):
+#########################################################################################################################################
+
+    elif(opcao == "2"):
         os.system('cls')
         while True:
             print("**************************************** CADASTRO DE VEICULOS ****************************************")
@@ -73,10 +91,9 @@ while True:
             print("1 - CADASTRAR NOVO VEICULO")
             print("0 - VOLTAR AO MENU ANTERIOR")
             print("\n")
-            opcao = int(input("DIGITE A OPÇÃO DESEJADA: "))
+            opcao = input("DIGITE A OPÇÃO DESEJADA: ")
             if(opcao == 1):
                 print("**************************************** CADASTRAR NOVO VEICULO ****************************************")
-                print("DADOS DO VEICULO")
                 tipoVeiculo = input("TIPO DE VEICULO.....: ")
                 fabricanteVeiculo = input("FABRICANTE..........: ")
                 modeloVeiculo = input("MODELO..............: ")
@@ -85,10 +102,11 @@ while True:
                 anoFabricaaoVeiculo = input("ANO DE FABRICAÇÃO...: ")
                 placaVeiculo = input("PLACA...............: ")
                 donoVeiculo = input("NOME DO DONO........: ")
+
                 os.system('cls')
-                print("**************************************** DADOS DO VEICULO CADASTRADOS COM SUCESSO ****************************************")
+
+                print(colorama.Fore.RED +"**************************************** DADOS DO VEICULO CADASTRADOS COM SUCESSO ****************************************"+ colorama.Style.RESET_ALL)
                 print("\n")
-                print("DADOS DO VEICULO")
                 print("TIPO DE VEICULO.....: ",tipoVeiculo)
                 print("FABRICANTE..........: ",fabricanteVeiculo)
                 print("MODELO..............: ",modeloVeiculo)
@@ -99,13 +117,14 @@ while True:
                 print("NOME DO DONO........: ",donoVeiculo)
                 print("\n")
 
-            elif(opcao == 0):
+            elif(opcao == "0"):
                 break
             else:
-                print("OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!")
+                print(colorama.Fore.RED +"OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!" + colorama.Style.RESET_ALL)
 
+#########################################################################################################################################
 
-    elif(opcao == 3):
+    elif(opcao == "3"):
         os.system('cls')
         while True:
             print("**************************************** OFICINA ON-LINE / AGENDAMENTOSS ****************************************")
@@ -113,30 +132,78 @@ while True:
             print("1 - INDICAR PROBLEMAS NO VEICULO E FAZER AGENDAMENTO")
             print("0 - VOLTAR AO MENU ANTERIOR")
             print("\n")
-            opcao = int(input("DIGITE A OPÇÃO DESEJADA: "))
+            opcao = input("DIGITE A OPÇÃO DESEJADA: ")
 
-            if(opcao == 1):
+            if(opcao == "1"):
                 print("**************************************** INDICAR PROBLEMAS NO VEICULO E FAZER AGENDAMENTO ****************************************")
-            elif(opcao == 0):
+                print("\n")
+                print("PROBLEMAS NO VEICULO")
+                problemaDescricao = input("DESCRIÇÃO DO PROBLEMA DO VEICULO..: ")
+                problemasPartes = input("PARTES AFETADAS.....................: ")
+                print("\n")
+                print("AGENDAR OFICINA")
+                problemaDia = input("DIGITE O DIA DO AGENDAMENTO.............: ")
+                problemaMes = input("DIGITE O MÊS DO AGENDAMENTO.............: ")
+                problemaAno = input("DIGITE O ANO DE AGENDAMENTO.............: ")
+                problemaPeriodo = input("PERÍODO DO DIA (MANHÃ OU TARDE).........: ")
+                print(colorama.Fore.RED +"ALERTA! A OFICINA ATENDE DAS 8H AS 20H DE SEGUNDA A SÁBADO!"+ colorama.Style.RESET_ALL)
+                problemaHorario = input("DIGITE O HORÁRIO DE ATENDIMENTO.........: ")
+                
+                os.system('cls')
+
+                print(colorama.Fore.RED +"**************************************** PROBLEMAS DO VEÍCULO E AGENDAMENTO REALIZADO COM SUCESSO ****************************************"+ colorama.Style.RESET_ALL)
+                print("\n")
+                print("DESCRIÇÃO DO PROBLEMA DO VEICULO............: ",problemaDescricao)
+                print("PARTES AFETADAS.............................: ",problemasPartes)
+                print("DIA DO AGENDAMENTO..........................: ",problemaDia)
+                print("MÊS DO AGENDAMENTO..........................: ",problemaMes)
+                print("ANO DE AGENDAMENTO..........................: ",problemaAno)
+                print("PERÍODO DO AGENDAMENTO (MANHÃ OU TARDE).....: ",problemaPeriodo)
+                print("HORÁRIO DE ATENDIMENTO......................: ",problemaHorario)
+
+            elif(opcao == "0"):
                 break
             else:
-                print("OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!")
+                print(colorama.Fore.RED +"OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!" + colorama.Style.RESET_ALL)
+
+#########################################################################################################################################  
     
-    
-    elif(opcao == 4):
+    elif(opcao == "4"):
         os.system('cls')
-        print("**************************************** ORÇAMENTO / PAGAMENTO ****************************************")
-    elif(opcao == 5):
+        while True:
+            print("**************************************** ORÇAMENTO / PAGAMENTO ****************************************")
+            print("\n")
+            print("1 - ORÇAMENTO E PAGAMENTO")
+            print("0 - VOLTAR AO MENU ANTERIOR")
+            print("\n")
+            opcao = input("DIGITE A OPÇÃO DESEJADA: ")
+
+            if(opcao == "1"):
+                print("**************************************** ORÇAMENTO / PAGAMENTO ****************************************")
+                print("\n")
+                print("PEÇAS USADAS")
+                input("PEÇAS 1............: ")
+                input("PREÇO: R$..: ")
+                input("PEÇAS 2............: ")
+                input("PREÇO: R$..: ")
+                input("PEÇAS 3............: ")
+                input("PREÇO: R$..: ")
+                
+                
+                
+
+
+    elif(opcao == "5"):
         os.system('cls')
         print("**************************************** INDICAÇÃO E CADASTRO DE NOVAS OFICINAS ****************************************")
-    elif(opcao == 6):
+    elif(opcao == "6"):
         os.system('cls')
         print("**************************************** PESQUISAR DADOS DE UM CLIENTE ****************************************")
-    elif(opcao == 0):
+    elif(opcao == "0"):
         os.system('cls')
         print("**************************************** SAIR DO SISTEMA ****************************************")
+        break
     else:
-        os.system('cls')
-        print("OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!")
+        print(colorama.Fore.RED + "OPÇÃO INCORRETA. DIGITE O APÇÃO CORRETA!" + colorama.Style.RESET_ALL)
 
 
