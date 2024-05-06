0a1daebc4aa7b9a48e96962359890534dd58ef51
carmipa <pabcarminati@gmail.com>
2024-05-06 17:37:44 -0300

    atualização pychar

--- exemplo-python-web/main.py
+++ exemplo-python-web/main.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 # explica para flask que a paste tem que ter nome
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='static')
 
 
 @app.route("/")
 
 def home():
-    return render_template("index.html")
+    return render_template("templates/index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
